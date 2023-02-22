d145075ac9bf09d7a9b51c60cda99b339c621d2e
Karunya <karunya.padala@ingwalabs.com>
2023-02-22 09:36:09 -0800

    Landing and searchResult page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import SearchPage from "./Pages/LandingPage/SearchPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/LandingPage" element={<LandingPage />} />
+          <Route path="/SearchPage" element={<SearchPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
