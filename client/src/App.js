e2492049bff477a6f1125f6506536e78a637c422
mounikapadala11 <119345122+mounikapadala11@users.noreply.github.com>
2023-02-27 18:38:43 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import EditCustomerProfilePage from "./Components/EditCustomerProfilePage";
+import EditLandlordProfilePage from "./Components/EditLandlordProfilePage";
 import LandingPage from "./Components/LandingPage";
-// import LandingPage1 from "./Components/LandingPage1";
+import WishlistPage from "./Components/WishlistPage";
 import SearchResult from "./Components/SearchResult";
 
 function App() {
@@ -18,6 +20,16 @@ function App() {
         <Routes>
           <Route path="/LandingPage" element={<LandingPage />} />
           <Route path="/SearchResult" element={<SearchResult />} />
+             <Route
+            path="/editCustomerProfilePage"
+            element={<EditCustomerProfilePage />}
+          />
+          <Route
+            path="/editLandlordProfilePage"
+            element={<EditLandlordProfilePage />}
+          />
+          <Route path="/AddListingPage" element={<AddListingPage />} />
+          <Route path="/wishlistPage" element={<WishlistPage />} />
         </Routes>
       </Router>
     </>
