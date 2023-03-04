534bb02222f1a4e4b45fc4e4639c6d15d06654fa
Karunya <karunya.padala@ingwalabs.com>
2023-03-04 11:29:23 -0800

    resolved conflicts in search resut and landing

--- client/src/Components/SearchResult.js
+++ client/src/Components/SearchResult.js
@@ -1,8 +1,8 @@
-import { Box, Heading, VStack, SimpleGrid } from "@chakra-ui/react";
+import { Box, Heading, VStack, SimpleGrid, Button } from "@chakra-ui/react";
 import NavBar from "./NavBar";
 import React from "react";
 import FilterRow from "./FilterRow";
-import ListingCard1 from "./ListingCard1";
+import ListingCard from "./ListingCard";
 import house1 from "../img/house1.jpg";
 import { getListingsByRating } from "../services/listingApis";
 
@@ -10,21 +10,37 @@ export default class SearchResult extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      recommendedListings: null
-    }
+      recommendedListings: null,
+      currentPage: 1,
+      listingsPerPage: 8,
+    };
   }
 
   async componentDidMount() {
     const recommendedListings = await getListingsByRating();
     this.setState({
-      recommendedListings
+      recommendedListings,
     });
-    console.log(recommendedListings, this.props?.history);
   }
 
-  
+  handlePageChange = (pageNumber) => {
+    this.setState({
+      currentPage: pageNumber,
+    });
+  };
 
   render() {
+    const { recommendedListings, currentPage, listingsPerPage } = this.state;
+    const indexOfLastListing = currentPage * listingsPerPage;
+    const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+    const currentListings = recommendedListings?.data.slice(
+      indexOfFirstListing,
+      indexOfLastListing
+    );
+    const totalPages = Math.ceil(
+      recommendedListings?.data.length / listingsPerPage
+    );
+
     return (
       <Box>
         <NavBar profileURL={"https://i.stack.imgur.com/l60Hf.png"}></NavBar>
@@ -36,20 +52,35 @@ export default class SearchResult extends React.Component {
         </Box>
         <Box margin="auto" pt={5} pl={10}>
           <VStack align="left" spacing={30}>
-            <SimpleGrid columns={{ base: 1, md: 4 }} spacing={10} mt={10} mx={10}>
-              {
-                this.state.recommendedListings?.data.map((listing, idx) => (
-                  <ListingCard1 key={idx} src={{...listing, img: house1}}> </ListingCard1>
-                ))
-              }
+            <SimpleGrid
+              columns={{ base: 1, md: 4 }}
+              spacing={10}
+              mt={10}
+              mx={10}
+            >
+              {currentListings?.map((listing, idx) => (
+                <ListingCard key={idx} src={{ ...listing, img: house1 }}>
+                  {" "}
+                </ListingCard>
+              ))}
             </SimpleGrid>
+            <Box mt={5}>
+              {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                (pageNumber) => (
+                  <Button
+                    key={pageNumber}
+                    mx={1}
+                    colorScheme={pageNumber === currentPage ? "blue" : "gray"}
+                    onClick={() => this.handlePageChange(pageNumber)}
+                  >
+                    {pageNumber}
+                  </Button>
+                )
+              )}
+            </Box>
           </VStack>
         </Box>
       </Box>
-      );
+    );
   }
 }
-
-
-
-      
--- server/models/index.js
+++ server/models/index.js
@@ -7,40 +7,40 @@ const loadDummyData = require("./script");
 const userSchema = require("./user");
 const wishlistSchema = require("./wishlist");
 
-module.exports = async ({$env}) => {
-    let connection;
+module.exports = async ({ $env }) => {
+  let connection;
 
-    try {
-        mongoose.set("strictQuery", true);
-        connection = await mongoose.connect($env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        if(!connection) {
-            throw new Error('Mongo Client not initialized');
-        }
-        console.log("Connected to Mongo DB", $env.DB_NAME);
-        const comment = connection.model('Comment', commentSchema);
-        const landlord = connection.model('Landlord', landlordSchema);
-        const like = connection.model('Like', likeSchema);
-        const listing = connection.model('Listing', listingSchema);
-        const user = connection.model('User', userSchema);
-        const wishlist = connection.model('Wishlist', wishlistSchema);
-        const model = {
-            comment,
-            landlord,
-            like,
-            listing,
-            user,
-            wishlist
-        }
-        // populate db
-        // loadDummyData(model);
-        return model;
-    } catch (error) {
-        console.log(error);
-        // close connection
-        console.log("Closing connection");
-        mongoose.connection.close();
+  try {
+    mongoose.set("strictQuery", true);
+    connection = await mongoose.connect($env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (!connection) {
+      throw new Error("Mongo Client not initialized");
     }
-}
+    console.log("Connected to Mongo DB", $env.DB_NAME);
+    const comment = connection.model("Comment", commentSchema);
+    const landlord = connection.model("Landlord", landlordSchema);
+    const like = connection.model("Like", likeSchema);
+    const listing = connection.model("Listing", listingSchema);
+    const user = connection.model("User", userSchema);
+    const wishlist = connection.model("Wishlist", wishlistSchema);
+    const model = {
+      comment,
+      landlord,
+      like,
+      listing,
+      user,
+      wishlist,
+    };
+    // populate db
+    // loadDummyData(model);
+    return model;
+  } catch (error) {
+    console.log(error);
+    // close connection
+    console.log("Closing connection");
+    mongoose.connection.close();
+  }
+};
