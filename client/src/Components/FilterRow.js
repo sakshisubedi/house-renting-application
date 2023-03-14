914dc70af4f10f1813acd3ad34bcf34976134180
JinHammer <hfj0108@163.com>
2023-03-14 12:43:59 -0700

    add flexibility to filter row

--- client/src/Components/FilterRow.js
+++ client/src/Components/FilterRow.js
@@ -17,6 +17,8 @@ import {
   MenuList,
   HStack,
   Icon,
+  Flex,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { FaChevronDown } from "react-icons/fa";
 
@@ -48,140 +50,147 @@ function FilterRow(props) {
 
   return (
     <Box my={5}>
-      <HStack spacing="20px">
-        <Box w="630px" h="75px" border="1px solid darkgray" borderRadius="15px">
-          <Input
-            id="search-input"
-            type="search"
-            placeholder="Location"
-            h="100%"
-            border="none"
-            fontSize="27px"
-            outline="none"
-            px={4}
-            onChange={handleInputChange}
-          />
-        </Box>
+      <Flex
+        minH={'75px'}
+        margin="auto">
+        <HStack spacing="20px">
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} w={{ base: "600px", md: "700px", lg: "850px" }} h="75px" border="1px solid darkgray" borderRadius="15px">
+              <Input
+                id="search-input"
+                type="search"
+                placeholder="Location"
+                h="100%"
+                border="none"
+                fontSize="27px"
+                outline="none"
+                px={4}
+                onChange={handleInputChange}
+              />
+          </Flex>
 
-        {/*rentPrice */}
-        <Select
-          placeholder="Rent"
-          h="75px"
+          {/*rentPrice */}
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} w="100%" maxW="160px">
+            <Select
+              placeholder="Rent"
+              h="75px"
 
-          defaultValue={rentPrice}
-          fontSize="25px"
-          border="1px solid #eaebef"
-          width="180px"
-          borderRadius="10px"
-          onChange={(e) => {
-            setRentPrice(e.target.value);
+              defaultValue={rentPrice}
+              fontSize="25px"
+              border="1px solid #eaebef"
+              borderRadius="10px"
+              onChange={(e) => {
+                setRentPrice(e.target.value);
 
-            props.search(postalCode, e.target.value, rating, beds, baths, petPref);
+                props.search(postalCode, e.target.value, rating, beds, baths, petPref);
 
-          }}
-          bg="#eaebef"
-        >
-          <option value="<1000">&lt;1000</option>
-          <option value="<2000">&lt;2000</option>
-          <option value="<3000">&lt;3000</option>
-          <option value="<4000">&lt;4000</option>
-          <option value="<5000">&lt;5000</option>
-        </Select>
+              }}
+              bg="#eaebef"
+            >
+              <option value="<1000">&lt;1000</option>
+              <option value="<2000">&lt;2000</option>
+              <option value="<3000">&lt;3000</option>
+              <option value="<4000">&lt;4000</option>
+              <option value="<5000">&lt;5000</option>
+            </Select>
+          </Flex>
 
-        {/*Ratings*/}
-        <Select
-          placeholder="Rating"
-          h="75px"
-          defaultValue={rating}
-          fontSize="25px"
-          border="1px solid #eaebef"
-          // w="max-content"
-          width="180px"
-          borderRadius="10px"
-          onChange={(e) => {
-            setRating(e.target.value);
+          {/*Ratings*/}
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} w="100%" maxW="160px">
+            <Select
+              placeholder="Rating"
+              h="75px"
+              defaultValue={rating}
+              fontSize="25px"
+              border="1px solid #eaebef"
+              // w="max-content"
+              borderRadius="10px"
+              onChange={(e) => {
+                setRating(e.target.value);
 
-            props.search(postalCode, rentPrice, e.target.value, beds, baths, petPref);
-          }}
-          bg="#eaebef"
-        >
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </Select>
+                props.search(postalCode, rentPrice, e.target.value, beds, baths, petPref);
+              }}
+              bg="#eaebef"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </Select>
+          </Flex>
 
-        {/*Beds*/}
-        <Select
-          placeholder="Beds"
-          h="75px"
-          defaultValue={beds}
-          fontSize="25px"
-          border="1px solid #eaebef"
-          w="max-content"
-          width="180px"
-          borderRadius="10px"
-          onChange={(e) => {
-            setBeds(e.target.value);
+          {/*Beds*/}
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} w="100%" maxW="160px">
+            <Select
+              placeholder="Beds"
+              h="75px"
+              defaultValue={beds}
+              fontSize="25px"
+              border="1px solid #eaebef"
+              borderRadius="10px"
+              onChange={(e) => {
+                setBeds(e.target.value);
 
-            props.search(postalCode, rentPrice, rating, e.target.value, baths, petPref);
+                props.search(postalCode, rentPrice, rating, e.target.value, baths, petPref);
 
-          }}
-          bg="#eaebef"
-        >
-          <option value="1">1 Bed</option>
-          <option value="2">2 Bed</option>
-          <option value="3">3 Bed</option>
-        </Select>
+              }}
+              bg="#eaebef"
+            >
+              <option value="1">1 Bed</option>
+              <option value="2">2 Bed</option>
+              <option value="3">3 Bed</option>
+            </Select>
+          </Flex>
 
-        {/*Baths*/}
-        <Select
-          placeholder="Bathrooms"
-          h="75px"
-          defaultValue={baths}
-          fontSize="25px"
-          border="1px solid #eaebef"
-          // w="max-content"
-          width="180px"
-          borderRadius="10px"
-          onChange={(e) => {
+          {/*Baths*/}
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} w="100%" maxW="160px">
+            <Select
+              placeholder="Bathrooms"
+              h="75px"
+              defaultValue={baths}
+              fontSize="25px"
+              border="1px solid #eaebef"
+              // w="max-content"
+              borderRadius="10px"
+              onChange={(e) => {
 
-            setBaths(e.target.value); 
-            props.search(postalCode, rentPrice, rating, beds, e.target.value, petPref);
+                setBaths(e.target.value);
+                props.search(postalCode, rentPrice, rating, beds, e.target.value, petPref);
 
-          }}
-          bg="#eaebef"
-        >
-          <option value="1">1 Bathroom</option>
-          <option value="2">2 Bathroom</option>
-          <option value="3">3 Bathroom</option>
-        </Select>
+              }}
+              bg="#eaebef"
+            >
+              <option value="1">1 Bathroom</option>
+              <option value="2">2 Bathroom</option>
+              <option value="3">3 Bathroom</option>
+            </Select>
+          </Flex>
 
-        {/*More*/}
+          {/*More*/}
+          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }} w="100%" maxW="160px">
+            <Select
+              placeholder="Pet Preferences"
+              h="75px"
+              defaultValue={petPref}
+              fontSize="25px"
+              border="1px solid #eaebef"
+              // w="max-content"
+              borderRadius="10px"
+              onChange={(e) => {
+                setPetPref(e.target.value);
 
-        <Select
-          placeholder="Pet Preferences"
-          h="75px"
-          defaultValue={petPref}
-          fontSize="25px"
-          width="180px"
-          border="1px solid #eaebef"
-          // w="max-content"
-          borderRadius="10px"
-          onChange={(e) => {
-            setPetPref(e.target.value);
-
-            props.search(postalCode, rentPrice, rating, beds, baths, e.target.value);
-          }}
-          bg="#eaebef"
-        >
-          <option value="Yes">Yes</option>
-          <option value="No">No</option>
-        </Select>
-
+                props.search(postalCode, rentPrice, rating, beds, baths, e.target.value);
+              }}
+              bg="#eaebef"
+            >
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+            </Select>
+          </Flex>
+        </HStack>
         {/* </Flex> */}
-      </HStack>
+      </Flex>
+
     </Box>
   );
 }
